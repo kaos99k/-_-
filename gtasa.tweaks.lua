0e0622b9a55895c1f66f7ad6bd9942bfeb7d7d88
kaos99k <58997823+kaos99k@users.noreply.github.com>
2023-11-23 10:10:19 +0000

    Update gtasa.tweaks.lua
    updated my lua thing; added additional code for custom veh', inspired by the oppressor from gtaV (it can shoot, just haven't worked out targeting yet. once finished, i'll upload as a seperate mod

--- gtasa.tweaks.lua
+++ gtasa.tweaks.lua
@@ -21,25 +21,42 @@ function main() wait(1000) printHelpString("lua tweaks")
 	
 	setFreeResprays( true )
 	setHeathazeEffect( false )
-		
-	while true do wait( 1 ) 
+	rx,ry=getScreenResolution()
+	
+	while true do wait( 0 ) 
 		if isPlayerPlaying( PLAYER_HANDLE ) then
+		
+			setPlayerNeverGetsTired(PLAYER_PED, true ) 	
+			setPlayerFastReload(PLAYER_PED, true ) 	
 			
+			--freezeOnscreenTimer(bool timer)
+			--
+			--setEveryoneIgnorePlayer(Player player, bool ignored) 	
+			--setCharNeverTargetted(Ped ped, bool untargetable) 	
+			
+
 		--	for i = 0, 9999 do if isButtonPressed( PLAYER_HANDLE, i ) then
 		--	printString( string.format("pressed: %i", i ), 100 ) end end
-				
+
 			L_LR, L_UD, R_LR, R_UD = getPositionOfAnalogueSticks( 0 )
 		--	if L_LR < -100 or L_LR > 100 then -- 0: ply1 1: ply2
 		--	printString( string.format( "pad: %i", L_LR ), 100 ) end
-			
-						
+					
+			if testCheat("ty") then bb,bx,by,bz=getTargetBlipCoordinates()
+				setCharCoordinates( PLAYER_PED,bx,by,bz )
+				--setCharCoordinatesDontWarpGang( PLAYER_PED, x,y,z )
+				--setCharCoordinatesDontWarpGangNoOffset(
+				--setCharCoordinatesNoOffset( PLAYER_PED, x,y,z )		
+			end
+				
 			if getCharHealth( PLAYER_PED ) < 125 then if regen >= 100 then 
 				damageChar( PLAYER_PED, -2, false ) regen=0 end end regen = regen+1 
 				
 			if testCheat("real") then if realtime
 				then realtime=false printString("Realtime OFF", 1000 )
 				else realtime=true printString("Realtime ON", 1000 ) end end
-			if realtime then setTimeOfDay( os.date("%H"), os.date("%M") ) end
+			if realtime then setTimeOfDay( os.date("%H"), os.date("%M") ) --end
+				freezeOnscreenTimer( true ) end 
 
 			if testCheat("coffee") then if coffee 
 				then coffee=false setGameGlobal( 1219, 0 ) printString("Hot Coffee OFF", 1000 )
@@ -78,69 +95,81 @@ function main() wait(1000) printHelpString("lua tweaks")
 							math.random(255), math.random(255), math.random(255), 128 )	
 					end end end
 
-			if isCharInAnyCar( PLAYER_PED ) then car=storeCarCharIsInNoSave( PLAYER_PED )
-				if isCharInFlyingVehicle( PLAYER_PED ) then giveWeaponToChar( PLAYER_PED, 46, 999 ) end
-				if isCharOnAnyBike( PLAYER_PED ) then bike=storeCarCharIsInNoSave( PLAYER_PED ) else bike=null end 		
+			if isCharInAnyCar(PLAYER_PED) then car=storeCarCharIsInNoSave(PLAYER_PED)
+				if isCharInFlyingVehicle(PLAYER_PED) then giveWeaponToChar(PLAYER_PED,46,1) end
+				if isCharOnAnyBike(PLAYER_PED) then bike=storeCarCharIsInNoSave(PLAYER_PED) else bike=null end 		
+				--if bike ~= null then warpCharIntoCar(PLAYER_PED,bike) setCharCanBeKnockedOffBike(PLAYER_PED,false) end --cleo steed mod works better
+				
+				renderDrawLine( 2.5, (ry*0.99), 2.5+getCarSpeed(car), (ry*0.99), 10, 0xFF990000 )
+				displayText( 2.5, ry/2.5, setFreeGxtEntry(string.format("%i", getCarSpeed(car) )) )
 
-				if isButtonPressed( PLAYER_HANDLE, 4 ) then mul=0.01 --and not isCharOnAnyBike( PLAYER_PED ) then -- alternoz
-					applyForceToCar( car, 
-						getCarForwardX(car)*mul, 
-						getCarForwardY(car)*mul, 
-						0,--getCarUprightValue(car)*mul, 
-						0, 
-						0, 
-						0 ) end
+				
+				if isCarModel( car, 12611 ) then --printString( string.format( "lua craft" ), 100 ) #
+				
+					setPlayerCanDoDriveBy(PLAYER_HANDLE,false) --giveWeaponToChar(PLAYER_PED,46,1)
+					pitch=getCarPitch(car) if pitch > 180 then pitch=pitch-360; end 
+					lift=(getCarMass(car)*0.00001)+(pitch*0.0001) --printString( string.format(":: %f", lift ), 100 )
 					
-				--angle = getCarHeading(Vehicle car) 
-				--roll = getCarRoll(Vehicle car) 
-				--pitch = getCarPitch(Vehicle car) 	
-				--value = getCarUprightValue(Vehicle car) 
+					if isButtonPressed( PLAYER_HANDLE, 4 ) then addToCarRotationVelocity( car, 0, 0, -L_LR*0.001 )
+						applyForceToCar( car,0,0, lift, 0,0,0 ) if isButtonPressed(PLAYER_HANDLE,16) then 
+							applyForceToCar( car, getCarForwardX(car)*0.01, getCarForwardY(car)*0.01, pitch*0.0001, 0,0,0 ) end end
+						
+					--ax,ay,az=getOffsetFromCarInWorldCoords(car,0,1,0) axx,ayy=convert3DCoordsToScreen(ax,ay,az)
+					--tx,ty,tz=getOffsetFromCarInWorldCoords(car,0,10,-pitch/4) txx,tyy=convert3DCoordsToScreen(tx,ty,tz)
+					--renderDrawLine(axx,ayy,txx,tyy,1,0xFF990000)
+					
+					--if isButtonPressed(PLAYER_HANDLE,17) then fireSingleBullet(ax,ay,az,tx,ty,tz,999999999) end
+						
+
+				else 
+					if isButtonPressed( PLAYER_HANDLE, 4 ) then -- alterNoz 
+						applyForceToCar( car, getCarForwardX(car)*0.01, getCarForwardY(car)*0.01, 0,0,0,0 ) end --giveNonPlayerCarNitro( car )
+				end 
+
 				
-				--angle = getCarMovingComponentOffset(Vehicle car) 	
-				--speed = getCarSpeed(Vehicle car) 	
-				--float vecX, float vecY, float vecZ = getCarSpeedVector(Vehicle car) 
-				
-				-- modelId = getCarModel(Vehicle veh)  	--noted for lua ship
-				
-				if isButtonPressed(PLAYER_HANDLE,18) then 
-					if not hop then if doesCarHaveHydraulics( car ) then 
-						setCarHydraulics( car, false ) else setCarHydraulics( car, true ) end 
-						hop=true end else if hop then hop=false end end
-				
+				if isButtonPressed(PLAYER_HANDLE,18) then if not hop then if doesCarHaveHydraulics(car) then 
+					setCarHydraulics(car,false) else setCarHydraulics(car,true) end hop=true end else if hop then hop=false end end
+					
+
 				--isCarStuckOnRoof(Vehicle car) then --flip the car
 				
 				--setCarLightsOn( car, true )
 
-				if testCheat("rep") then fixCar( car ) end
-				
-				if testCheat("drift") then if drift 
-				then drift=false printString("Drift OFF", 1000 ) 
-				else drift=true printString("Drift ON", 1000 ) end end
-				if drift then --setCarCollision( car, true )
-					if isButtonPressed(PLAYER_HANDLE,17) and isVehicleOnAllWheels(car) then 
-						--setCarCollision( car, false )
-						addToCarRotationVelocity( car, 0, 0, -L_LR/1500 )
-						--if isCarInAirProper( car ) then setCarCollision( car, true ) end
-					end
-				--else if isButtonPressed( PLAYER_HANDLE , 4 ) then giveNonPlayerCarNitro( car ) end
-				end
-				
 				--if isCarOnFire( car ) then taskEveryoneLeaveCar( car ) bike=null car=null end
 				
-				
-				if isPlayerControlOn(PLAYER_HANDLE) and isButtonPressed(PLAYER_HANDLE,4) and isButtonPressed(PLAYER_HANDLE,15) then 
+				if isPlayerControlOn(PLAYER_HANDLE) 
+					and isButtonPressed(PLAYER_HANDLE,4) and isButtonPressed(PLAYER_HANDLE,15) then 
 					
+					--taskEveryoneLeaveCar( car )
 					taskLeaveCarImmediately( PLAYER_PED, car )
-					removeCharFromCarMaintainPosition( PLAYER_PED, car )
+					--removeCharFromCarMaintainPosition( PLAYER_PED, car )
 					
 					--cx, cy, cz = getOffsetFromCarInWorldCoords( car, 0.0, 0.0, 2.0 )
 					--warpCharFromCarToCoord( PLAYER_PED, cx, cy, cz )
 					
 					bike=null car=null end
-			
+					
+				if testCheat("diy") then bb,bx,by,bz=getTargetBlipCoordinates()
+					carGotoCoordinates(car,bx,by,bz) 
+					
+					--setCarForwardSpeed(car,100)
+					
+					--carGotoCoordinatesAccurate(car,bx,by,bz)
+					--carGotoCoordinatesRacing(car,bx,by,bz)
+					--taskGoToCoordAnyMeans(PLAYER_PED,bx,by,bz,0,car)
+					
+				end
+				
+				if testCheat("drift") then if drift 
+				then drift=false printString("Drift OFF",100) else drift=true printString("Drift ON",100) end end
+				if drift then if isButtonPressed(PLAYER_HANDLE,17) and isVehicleOnAllWheels(car) then 
+					addToCarRotationVelocity( car, 0, 0, -L_LR/1500 ) end end
+
+				if testCheat("rep") then fixCar( car ) end
+				
 				
 			else 
-
+			
 				if testCheat( "gg" ) then
 					--drop weapon
 					if pickup ~= null then removePickup( pickup ) end 
@@ -169,11 +198,10 @@ function main() wait(1000) printHelpString("lua tweaks")
 					vehicleDoesProvideCover( car, true )
 					--addBlipForCar( car )
 				end
+				
+				if testCheat("sav") then activateSaveMenu() end
 			
-		--		if bike ~= null then fixCar( bike )
-		--			warpCharIntoCar( PLAYER_PED, bike )
-		--			setCharCanBeKnockedOffBike( PLAYER_PED, false )
-		--		end	-- FX Steed.cs works better --
+		
 	
 			end					
 	    end
